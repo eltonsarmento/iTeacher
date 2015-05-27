10cdc35f5a7ecb8b874efe87aebf08562018637e
elton <=>
2015-05-27 14:43:59 -0300

    teste com nome setado

--- market/_sources/_data/administradores.dao.php
+++ market/_sources/_data/administradores.dao.php
@@ -1,5 +1,4 @@
 <?php
-// ===================================================================
 require_once(dirname(__FILE__) . '/usuarios.dao.php');
 // ===================================================================
 class AdministradoresDAO extends UsuariosDAO {
